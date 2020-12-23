bca8e5c7b540cf225ea213e5258e343087a753cc
Abire Mahfouze <abiremahfouze@gmail.com>
2020-12-23 10:07:01 +0000

    premiere commit

---
+++ app/.flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=init__.py
+FLASK_ENV=development
---
+++ app/init__.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template,request, url_for, flash, redirect
+import sqlite3
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your secret key'
+
+    
+    @app.route('/')
+    def index():
+        conn = get_db_connection()
+        movies = conn.execute('select * from movies order by movieId DESC limit 10 ').fetchall()
+        conn.close()
+
+        return render_template('index.html',movies=movies)
+
+    def get_db_connection():
+        conn = sqlite3.connect('data/moviesdb.db')
+        conn.row_factory = sqlite3.Row
+        return conn
+    
+   
+    @app.route('/add')
+    def create():
+        return render_template('create.html')
+    
+    
+    
+    @app.route('/create', methods=('GET', 'POST'))
+    def create_movies():
+        if request.method == 'POST':
+            title = request.form['title']
+            genre = request.form['genre']
+
+            if not title:
+                flash('Title is required!')
+            else:
+                conn = get_db_connection()
+                conn.execute('INSERT INTO movies (title, genres) values (?, ?)',
+                            (title, genre))
+                conn.commit()
+                conn.close()
+                return redirect(url_for('index'))
+
+        return render_template('index.html')
+    
+    
+    return app
